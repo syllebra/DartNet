53fffbe74042a7efe63e213a774f49d200cdd331
syllebra <syllebra@free.fr>
2024-05-17 13:41:01 -1000

    yolov8 can now be revert and using no calibration points

--- generator/dataset_to_yolov8.py
+++ generator/dataset_to_yolov8.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 import pathlib
 import json
 import numpy as np
+import glob
 
 def get_data_file(image_file):
     file= os.path.splitext(image_file)[0]+".json"
@@ -43,6 +44,14 @@ def reorganize_images_data(directory, validation_ratio, test_ratio):
             dst = "train"
         move_sample(f,os.path.join(directory, "images",dst))
 
+def revert_initial_data(directory):
+    # Get List of all images
+    files = glob.glob(directory + '/**/*.jpg', recursive=True)
+
+    for f in tqdm(files):
+         if(os.path.abspath(os.path.dirname(f)) != os.path.abspath(directory)):
+            move_sample(f,directory)
+
 def get_image_size(file):
     from PIL import Image
     im = Image.open(file)
@@ -50,7 +59,7 @@ def get_image_size(file):
 import os
 import struct
 
-def translate_annotations(directory, width=None, height=None):
+def translate_annotations(directory, width=None, height=None, no_cals=False):
     os.makedirs(os.path.join(directory, "labels"), exist_ok=True)
     tmp = pathlib.Path(directory)
     for p in tqdm(list(tmp.rglob("*.json")), leave=False):
@@ -75,12 +84,12 @@ def translate_annotations(directory, width=None, height=None):
                     cl = 0
                 elif ("cal" in k.lower()):
                     cl = int(k.replace("cal","").strip())
-
-                if(v[0]>=0 and v[0]<width and v[1]>0 and v[1]<height):
-                    outfile.write(f"{cl} {v[0]/width} {v[1]/height} {sz} {sz}\n")
+                if(not no_cals or cl <1):
+                    if(v[0]>=0 and v[0]<width and v[1]>0 and v[1]<height):
+                        outfile.write(f"{cl} {v[0]/width} {v[1]/height} {sz} {sz}\n")
             for k,v in data["bbox"].items():
                 if("dart" in k.lower()):
-                    cl = 5
+                    cl = 1 if no_cals else 5
 
                 if(v[0][0] <0 and v[1][0] <0): continue
                 if(v[0][0] >=width and v[1][0] >=width): continue
@@ -102,30 +111,45 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Generate a realistic random darts dataset.')
     parser.add_argument("-d", "--directory", type=str, default="_GENERATED", help="Destinataion directory")
-    
-    args = parser.parse_args()
-    print(args)
-    
-    directory = args.directory
-    validation_ratio = 0.1
-    test_ratio = 0.05
-    reorganize_images_data(directory,validation_ratio, test_ratio)
-    translate_annotations(directory, width=None, height=None)
-    with open(os.path.join(directory,"data.yml"),"w") as outfile:
-        outfile.write(f"path: {directory}\n")
-        outfile.write(f"train: images/train\n")
-        outfile.write(f"val: images/val\n")
-        if(test_ratio>0):
-            outfile.write(f"test: images/test\n")
+    parser.add_argument('--revert', default=False, action=argparse.BooleanOptionalAction)
+    parser.add_argument('--no_cals', default=False, action=argparse.BooleanOptionalAction)
+    parser.set_defaults(revert=False)
 
-        outfile.write(f"\n# Classes\n")
-        outfile.write(f"names:\n")
-        outfile.write(f" 0: tip\n")
-        outfile.write(f" 1: cal1\n")
-        outfile.write(f" 2: cal2\n")
-        outfile.write(f" 3: cal3\n")
-        outfile.write(f" 4: cal4\n")
-        outfile.write(f" 5: dart\n")
+    args = parser.parse_args()
+    
+    
+    if(args.revert):
+        revert_initial_data(args.directory)
+        if(os.path.exists(os.path.join(args.directory,"data.yml"))):
+            os.remove(os.path.join(args.directory,"data.yml"))
+        if(os.path.exists(os.path.join(args.directory,"images"))):
+            shutil.rmtree(os.path.join(args.directory,"images"))
+        if(os.path.exists(os.path.join(args.directory,"labels"))):
+            shutil.rmtree(os.path.join(args.directory,"labels"))
+    else:
+        directory = args.directory
+        validation_ratio = 0.1
+        test_ratio = 0.05
+        reorganize_images_data(directory,validation_ratio, test_ratio)
+        translate_annotations(directory, width=None, height=None, no_cals=args.no_cals)
+        with open(os.path.join(directory,"data.yml"),"w") as outfile:
+            outfile.write(f"path: {directory}\n")
+            outfile.write(f"train: images/train\n")
+            outfile.write(f"val: images/val\n")
+            if(test_ratio>0):
+                outfile.write(f"test: images/test\n")
+
+            outfile.write(f"\n# Classes\n")
+            outfile.write(f"names:\n")
+            outfile.write(f" 0: tip\n")
+            if(not args.no_cals):
+                outfile.write(f" 1: cal1\n")
+                outfile.write(f" 2: cal2\n")
+                outfile.write(f" 3: cal3\n")
+                outfile.write(f" 4: cal4\n")
+                outfile.write(f" 5: dart\n")
+            else:
+                outfile.write(f" 1: dart\n")
 
 
     
