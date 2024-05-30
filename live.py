88d30f377a4b1e0efe7e830709ffef5f0ebaff6b
syllebra <syllebra@free.fr>
2024-05-30 09:57:31 -1000

    Add auto lock mechanism for steady setup

--- live.py
+++ live.py
@@ -17,14 +17,15 @@ from videocapture import ScreenVideoCapture
 #cap = cv2.VideoCapture("./datasets/real/vid/winmau_blade_6_A.mp4")
 #cap = cv2.VideoCapture("./datasets/real/vid/output3.avi")
 #cap = cv2.VideoCapture("./datasets/real/vid/winmau_blade_6_C.avi")
-cap = ScreenVideoCapture(pick=True)
+#cap = ScreenVideoCapture(pick=True)
 
-time_mult=1.5#0.0001
+time_mult=0.25#0.0001
 fps = 21.0
 
-#board_img_path = 'generator/3D/Boards/canaveral_t520.jpg'
+board_img_path = 'generator/3D/Boards/canaveral_t520.jpg'
 #board_img_path = 'generator/3D/Boards/unicorn-eclipse-hd2.jpg'
-board_img_path = 'generator/3D/Boards/winmau_blade_6.jpg'
+#board_img_path = 'generator/3D/Boards/winmau_blade_6.jpg'
+cap = cv2.VideoCapture("./datasets/real/vid/home01.avi")
 board = Board(board_img_path.replace(".jpg",".json"))
 
 with open(board_img_path.replace(".jpg",".json")) as f:
@@ -32,7 +33,7 @@ with open(board_img_path.replace(".jpg",".json")) as f:
 
 detector = TargetDetector(board_img_path)
 
-status_colors = {"not_detected": (0,0,255), "detected": (0,255,0), "opencv_detected": (0,160,75)}
+status_colors = {"not_detected": (0,0,255), "detected": (0,255,0), "opencv_detected": (0,160,75), "locked":(160,160)}
 
 # cap.set(3, 640)
 # cap.set(4, 480)
@@ -47,7 +48,12 @@ model = YOLO("best_s_tip_boxes640_B.pt")
 model_train_size = 640
 force_opencv_detector = True
 use_clahe = False
-stablize = False
+stablize = True
+
+locked_target_delay = 10 # 10 frames without moving => stable
+locked_frames = 0
+locked = False
+last_cal_pts = None
 
 # object classes
 classNames = ["tip", "cal1", "cal2", "cal3", "cal4", "dart"]
@@ -149,8 +155,8 @@ while True:
     pts_cal = find_cal_pts(res)
 
     opencv_detected = False
-    if(force_opencv_detector or pts_cal is None):
-        ts = time.time()
+    if(not locked and (force_opencv_detector or pts_cal is None)):
+        tps = time.time()
         found_cals, M, conf = detector.detect(img)
         if(M is not None):
             pts_cal = found_cals
@@ -158,15 +164,30 @@ while True:
                 v = {"x1": p[0]-10, "y1": p[1]-10,"x2": p[0]+10, "y2": p[1]+10, 'conf':conf, "cls":i+1}
                 res.append(v)
             opencv_detected = True
-        print(f"OpenCV target Detector: {int((time.time()-ts)*1000)} ms")
+        print(f"OpenCV target Detector: {int((time.time()-tps)*1000)} ms")
 
     res = filter_res(res)
-    pts_cal = find_cal_pts(res)
+    
+    if(locked):
+        pts_cal = last_cal_pts
+    else:
+        pts_cal = find_cal_pts(res)
 
     if(cropped and pts_cal_dst is None and pts_cal is not None):
         pts_cal_dst = pts_cal.copy()
 
-    if(stablize and pts_cal is not None and pts_cal_dst is not None):
+    # check if moved
+    if(locked_target_delay>0 and not locked):
+        if(last_cal_pts is not None):
+            diff = (last_cal_pts-pts_cal)
+            moved = np.max(diff)
+            if(moved <2):
+                locked_frames += 1
+                if(locked_frames>=locked_target_delay):
+                    locked = True
+        last_cal_pts = pts_cal
+
+    if(locked and stablize and pts_cal is not None and pts_cal_dst is not None):
         M = cv2.getPerspectiveTransform(pts_cal.astype(np.float32), pts_cal_dst.astype(np.float32))
         im = cv2.warpPerspective(img.copy(), M, (img.shape[1],img.shape[0]))
 
@@ -250,8 +271,6 @@ while True:
                 # put box in cam
                 cv2.rectangle(img, (x1, y1), (x2, y2), box_cols[cls], 1)
 
-
-
                 text = f"{classNames[cls]} ({confidence:.2f})"
                 score = False
                 if(cls == 0):
@@ -269,6 +288,8 @@ while True:
 
                 cv2.putText(img, text, org, font, fontScale, color, thickness)
 
+            for p in pts_cal:
+                cv2.circle(img, p.astype(np.int32),4,(200,180,60),cv2.FILLED)
         
         img = cv2.copyMakeBorder(img, 10, 10, 10, 10, cv2.BORDER_ISOLATED,value=status_colors[status])
         return img
@@ -276,6 +297,8 @@ while True:
     status = "not_detected"
     if(pts_cal is not None):
         status = "opencv_detected" if opencv_detected else "detected"
+        if(locked):
+            status = "locked"
     img = draw(img, res, status=status)
     
     # res2 = infer(img,model2)
